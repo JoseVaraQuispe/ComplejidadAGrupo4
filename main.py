166fe055d8ae4df27e8f1496f75f334ca46b05cf
jose.vara <jose.vara@vcaperu.com>
2023-11-10 09:54:58 -0500

    mainprogram moviedle

---
+++ main.py
@@ -0,0 +1,13 @@
+import Tkinter as tk
+
+# Crear la ventana
+ventana = tk.Tk()
+
+# Configurar el tamao de la ventana
+ventana.geometry("800x600")
+
+# Configurar el ttulo de la ventana
+ventana.title("Moviedle")
+
+# Iniciar el bucle principal de la interfaz grfica
+ventana.mainloop()
